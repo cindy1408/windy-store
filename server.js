8d9ca05c96525eccaad7a2b34a67b7b6379a4849
Cindy Cheung <cindycheung1408@gmail.com>
2021-06-15 14:18:51 +0100

    stripe-checkout-backend

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ const express = require("express");
 // const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const shortid = require("shortid");
-
-
-
+const stripe = require(stripe)('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')
 
 const app = express();
 
@@ -50,6 +48,49 @@ app.get("/api/products", async (req, res) => {
      const newProduct = new Product(req.body);
      const savedProduct = await newProduct.save();
      res.send(savedProduct);
+ });
+
+ app.post("/checkout", async(req, res) => {
+     console.log("request", res.body);
+     let error; 
+     let status; 
+     try{
+         const {product, token} = req.body;
+
+         const customer = await 
+         stripe.customers.create({
+             email: token.email,
+             source: token.id
+         });
+         const idempotency_key = uuid();
+         const charge = await stripe.charges.create({
+             amount: product.price*100,
+             currency: "gbp",
+            customer: customer.id,
+            receipt_email: token.email,
+            description: `Purchased the ${product.name}`,
+            shipping: {
+                name: token.card.name,
+                address: {
+                    line1: token.card.address_line1,
+                    line2: token.card.address_line2,
+                    city: token.card.address_city,
+                    country: token.card.address_country, 
+                    postal_code: token.card.address_zip
+                }
+            }
+         }, 
+         {
+            idempotency_key
+         }
+         );
+         console.log(`Charge: `, {charge});
+         status: "success";
+     } catch (error){
+         console.error("Error: ", error);
+         status: "failure"; 
+     }
+     res.json({error, status})
  });
 
  app.delete("/api/products/:id", async(req, res) => {
