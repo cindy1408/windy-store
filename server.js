417ef6a25d87975fcd50ae0f33108c9bb4b42d6b
Cindy Cheung <cindycheung1408@gmail.com>
2021-06-26 17:30:52 +0100

    updated

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
+const cors = require('cors');
 const express = require("express");
-// const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const shortid = require("shortid");
-const stripe = require(stripe)('')
-
 const app = express();
+//const stripe = require(stripe)('sk_test_51IzITeCp2VW1FwclE62hzANw1I2Suy2WSil8ziMrBbXIDc92O5NjTW11BjnhlYqYmxoqrO4PpT19rOV5ntrlxOHh008cUhK9tk')
 
 app.use(express.json());
+
+const allowedOrigins = ['http://localhost:5000'];
+
+const corsOptions = {
+     origin: allowedOrigins
+ }
+
+ app.use(cors(corsOptions));
+
+
 
 const port = process.env.PORT || 5000; 
 
@@ -36,12 +46,11 @@ app.listen(port, () => console.log(`serve at http://localhost:${port}`));
      })
  );
 
-//creating new endpoints
-//app.get("/api/products", async(req, res) => res.send('Hello!'))
- 
+
 app.get("/api/products", async (req, res) => {
- const products = await Product.find({});
- res.send(products);
+    res.json({msg: 'This is CORS-enabled for only example.com.'})
+    const products = await Product.find({});
+    res.send(products);
  });
 
  app.post("/api/products", async(req, res) => {
@@ -84,7 +93,7 @@ app.get("/api/products", async (req, res) => {
             idempotency_key
          }
          );
-         console.log(`Charge: `, {charge});
+         console.log(`Charge:`, {charge});
          status: "success";
      } catch (error){
          console.error("Error: ", error);
@@ -93,7 +102,45 @@ app.get("/api/products", async (req, res) => {
      res.json({error, status})
  });
 
+
  app.delete("/api/products/:id", async(req, res) => {
      const deletedProduct = await Product.findByIdAndDelete(req.params.id);
      res.send(deletedProduct);
  })
+
+ const Order = mongoose.model('order', new mongoose.Schema(
+     {
+        _id: {
+            type: String,
+            default: shortid.generate
+        }, 
+    email: String,
+    name: String, 
+    address: String,
+    total: Number,
+    cartItems: [{
+        _id: String, 
+        title: String,
+        price: Number, 
+        count: Number
+    },
+    ],
+    }, 
+    {
+        timestamps: true,
+    }
+));
+
+app.post("/api/orders", async(req, res) => {
+    if(
+        !res.body.name || 
+        !res.body.email ||
+        !res.body.address ||
+        !res.body.total ||
+        !res.body.cartItems
+        ){
+            return res.send({message: "Data is required"});
+        }
+        const order = await Order(req.body).save();
+        res.send(order);
+})
