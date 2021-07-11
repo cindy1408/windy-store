ca222cd8ae5fc76c910d0f78f3f8e455b75b8d2a
Cindy Cheung <cindycheung1408@gmail.com>
2021-07-11 12:12:10 +0100

    reset

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 // const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const shortid = require("shortid");
-const stripe = require(stripe)('')
-
+// const stripe = require(stripe)('sk_test_51IzITeCp2VW1FwclE62hzANw1I2Suy2WSil8ziMrBbXIDc92O5NjTW11BjnhlYqYmxoqrO4PpT19rOV5ntrlxOHh008cUhK9tk')
 const app = express();
 
+//Recognises the incoming Request Object as a JSON object, this is called as a middleware.. 
 app.use(express.json());
 
 const port = process.env.PORT || 5000; 
@@ -41,14 +41,23 @@ app.listen(port, () => console.log(`serve at http://localhost:${port}`));
  
 app.get("/api/products", async (req, res) => {
  const products = await Product.find({});
+//  res.send('Hello World')
  res.send(products);
  });
+
+
+ app.get('/secret', async (req, res) => {
+    const intent = fetch // ... Fetch or create the PaymentIntent
+    res.json({client_secret: intent.client_secret});
+  });
+
 
  app.post("/api/products", async(req, res) => {
      const newProduct = new Product(req.body);
      const savedProduct = await newProduct.save();
      res.send(savedProduct);
  });
+
 
  app.post("/checkout", async(req, res) => {
      console.log("request", res.body);
